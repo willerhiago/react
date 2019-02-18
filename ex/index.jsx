14cbc3c1cfb8e2771199b6d87dd3057295934b46
willerhiago <willer.hiago@gmail.com>
2019-02-18 00:16:58 -0300

    commit

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Olá React2</h1>, document.getElementById('app'))
