bc67719e265f9e5ebf09165c4c41a9c455302331
willerhiago <willer.hiago@gmail.com>
2019-02-18 01:29:01 -0300

    commit

--- ex.2/index.jsx
+++ ex.2/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Component from './component'
 
-ReactDOM.render(<Component class = 'teste' value= 'show'/>, document.getElementById('app'))
+ReactDOM.render(<Component/>, document.getElementById('app'))
